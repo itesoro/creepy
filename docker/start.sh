a90327d27a62643293ac424d2bb80c614a7729a9
Roman Rizvanov <progbeat@users.noreply.github.com>
2020-11-16 12:39:56 +0200

    Bugfixes (#2)
    * First commit.
    
    * Save.
    
    * Bugfixes.
    
    * Add setup.cfg.
    
    * Add setup.py.
    
    * Pass authorized_keys into container without hacks. Mount /blobs.
    
    * Don't cache creepy installing.
    
    * Update README.md.
    
    * Send files and directories.
    
    * Proxy magic calls. Add hello world example.
    
    * flake8 config.
    
    * Handshake protocol.
    
    * Minor fixes.
    
    * Raise exceptions in handshake_hi().
    
    * Bugfixes.
    
    * Name fix.
    
    * Format fix. Remove debug output.
    
    * Nonces on transport level.
    
    * Add delays for bad requests. Fix status codes.
    
    * Fix password.
    
    * Minor fixes.
    
    * Rename crypto -> protocol.
    
    * Change handshake halt size to 16.
    
    * Add TODO.
    
    * Remove unused code.
    
    * Fix some hardcoded constants.
    
    * debug=False.
    
    * CHUNK_SIZE = 2**24.
    
    * Minor fixes.
    
    * Fix start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -6,5 +6,4 @@ docker run -it \
     -p 8000:8000 \
     --mount type=bind,source=$(realpath ~/.ssh/authorized_keys),target=/root/.ssh/authorized_keys,readonly \
     --mount source=blobs,destination=/blobs \
-    bash
-    # creepy uvicorn creepy:app
+    creepy bash
