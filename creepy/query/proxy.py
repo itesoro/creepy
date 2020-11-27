b335fcef59b3cf55568002a945399467d9dcd6c9
Roman Rizvanov <progbeat@gmail.com>
2020-11-27 20:54:11 +0300

    Simplify.

--- creepy/query/proxy.py
+++ creepy/query/proxy.py
@@ -77,12 +77,6 @@ def _catch_magic_call_nd_download(name):
     return handler
 
 
-def _proxy__del__(self):
-    id = self._id
-    if id > 0:
-        self._remote._lazy_delete(id)
-
-
 def _make_child(self, query):
     remote = self._remote
     result = remote._post(query)
@@ -93,59 +87,12 @@ def _make_child(self, query):
     return ProxyObject(remote, *result)
 
 
-def _proxy__getattr__(self, name):
-    return _make_child(self, GetattrQuery(self._id, name))
-
-
-def _proxy__setattr__(self, name, value):
-    self._remote._post(SetattrQuery(self._id, name, value))
-
-
-def _proxy__delattr__(self, name):
-    self._remote._post(DelattrQuery(self._id, name))
-
-
 def _proxy__call__(self, *args, **kwargs):
     return _make_child(self, CallQuery(self._id, args, kwargs))
 
 
-def _magics():
-    magics = [
-        '__abs__', '__abstractmethods__', '__add__', '__aiter__', '__and__', '__anext__', '__await__', '__ceil__',
-        '__contains__', '__delattr__', '__delitem__', '__dir__', '__divmod__', '__enter__', '__eq__', '__exit__',
-        '__floor__', '__floordiv__', '__format__', '__ge__', '__getformat__', '__getitem__', '__getnewargs1__', '__gt__',
-        '__hash__', '__iadd__', '__iand__', '__imul__', '__index__', '__invert__', '__ior__', '__isub__', '__iter__',
-        '__ixor__', '__le__', '__len__', '__lshift__', '__lt__', '__mod__', '__module__', '__mul__', '__ne__',
-        '__neg__', '__next__', '__or__', '__pos__', '__pow__', '__radd__', '__rand__', '__rdivmod__', '__reduce1__',
-        '__reduce1_ex__', '__reversed__', '__rfloordiv__', '__rlshift__', '__rmod__', '__rmul__', '__ror__', '__round__',
-        '__rpow__', '__rrshift__', '__rshift__', '__rsub__', '__rtruediv__', '__rxor__', '__setformat__', '__setitem__',
-        '__sizeof__', '__sub__', '__subclasshook__', '__truediv__', '__trunc__', '__xor__'
-    ]
-    return_primitive_magics = [
-        '__bool__', '__complex__', '__float__', '__instancecheck__', '__int__', '__repr__', '__str__',
-        '__subclasscheck__'
-    ]
-    default_flags = 0
-    namespace = {
-        '__del__': _proxy__del__,
-        '__getattr__': _proxy__getattr__,
-        '__setattr__': _proxy__setattr__,
-        '__delattr__': _proxy__delattr__,
-        '__call__': _proxy__call__
-    }
-    for name in magics:
-        namespace[name] = _catch_magic_call(name)
-    for name in return_primitive_magics:
-        namespace[name] = _catch_magic_call_nd_download(name)
-    bit = 1
-    for name, fn in namespace.items():
-        namespace[name] = (fn, bit)
-        default_flags |= bit
-        bit <<= 1
-    return namespace, default_flags
-
-
-_magics, _default_flags = _magics()
+def _replace_with_result(fn):
+    return fn()
 
 
 def _memorize(f):
@@ -173,14 +120,18 @@ def _make_proxy_class_namespace(flags):
 
 @_memorize
 def _make_proxy_class(flags, class_name):
-    cls = ProxyObject
+    base_proxy_cls = ProxyObject
     namespace = _make_proxy_class_namespace(flags)
-    return type(f'{cls.__name__}[{class_name}]', (cls,), namespace)
+    if class_name is not None:
+        proxy_class_name = f'{base_proxy_cls.__name__}[{class_name}]'
+    else:
+        proxy_class_name = base_proxy_cls.__name__
+    return type(proxy_class_name, (base_proxy_cls,), namespace)
 
 
 @_memorize
 def proxy_flags(cls):
-    flags = 15
+    flags = 0
     for name in dir(cls):
         v = _magics.get(name)
         if v is not None:
@@ -196,9 +147,60 @@ def _with_flags(value, id):
 class ProxyObject:
     __slots__ = ('_remote', '_id')
 
-    def __new__(cls, remote, id, flags=_default_flags, class_name='Unknown'):
+    def __new__(cls, remote, id, flags=-1, class_name=None):
         proxy_cls = _make_proxy_class(flags, class_name)
         ins = object.__new__(proxy_cls)
         object.__setattr__(ins, '_remote', remote)
         object.__setattr__(ins, '_id', id)
         return ins
+
+    def __del__(self):
+        id = self._id
+        if id > 0:
+            self._remote._lazy_delete(id)
+
+    def __getattr__(self, name):
+        return _make_child(self, GetattrQuery(self._id, name))
+
+    def __setattr__(self, name, value):
+        self._remote._post(SetattrQuery(self._id, name, value))
+
+    def __delattr__(self, name):
+        self._remote._post(DelattrQuery(self._id, name))
+
+
+@_replace_with_result
+def _magics():
+    magics = [
+        '__abs__', '__abstractmethods__', '__add__', '__aiter__', '__and__', '__anext__', '__await__', '__ceil__',
+        '__contains__', '__delitem__', '__dir__', '__divmod__', '__enter__', '__eq__', '__exit__',
+        '__floor__', '__floordiv__', '__format__', '__ge__', '__getformat__', '__getitem__', '__gt__',
+        '__hash__', '__iadd__', '__iand__', '__imul__', '__index__', '__invert__', '__ior__', '__isub__', '__iter__',
+        '__ixor__', '__le__', '__len__', '__lshift__', '__lt__', '__mod__', '__module__', '__mul__', '__ne__',
+        '__neg__', '__next__', '__or__', '__pos__', '__pow__', '__radd__', '__rand__', '__rdivmod__',
+        '__reversed__', '__rfloordiv__', '__rlshift__', '__rmod__', '__rmul__', '__ror__', '__round__',
+        '__rpow__', '__rrshift__', '__rshift__', '__rsub__', '__rtruediv__', '__rxor__', '__setformat__', '__setitem__',
+        '__sizeof__', '__sub__', '__subclasshook__', '__truediv__', '__trunc__', '__xor__'
+    ]
+    return_primitive_magics = [
+        '__bool__', '__complex__', '__float__', '__instancecheck__', '__int__', '__repr__', '__str__',
+        '__subclasscheck__'
+    ]
+    always_magics = {'__instancecheck__', '__repr__', '__str__', '__subclasscheck__'}
+    namespace = {'__call__': _proxy__call__}
+
+    def add_to_namespace(name, fn):
+        if name in always_magics:
+            setattr(ProxyObject, name, fn)
+        else:
+            namespace[name] = fn
+
+    for name in magics:
+        add_to_namespace(name, _catch_magic_call(name))
+    for name in return_primitive_magics:
+        add_to_namespace(name, _catch_magic_call_nd_download(name))
+    bit = 1
+    for name, fn in namespace.items():
+        namespace[name] = (fn, bit)
+        bit <<= 1
+    return namespace
