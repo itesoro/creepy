ae5f8a95ce95e37644eb498e29d81a46d08e231f
Roman Rizvanov <progbeat@gmail.com>
2021-01-21 15:55:24 +0300

    Add deamon.sh.

---
+++ docker/daemon.sh
@@ -0,0 +1,23 @@
+DOCKER_IMAGE=creepy
+
+if which nvidia-smi >> /dev/null; then
+    DOCKER_ARGS="--gpus all"
+else
+    DOCKER_ARGS=
+fi
+
+try_bind () {
+    local p=$(realpath $1)
+    if [ -f "$p" ]; then
+        DOCKER_ARGS="$DOCKER_ARGS --mount type=bind,source=$p,target=$2,readonly"
+    fi
+}
+
+try_bind ~/.ssh/authorized_keys /root/.ssh/authorized_keys
+
+docker run -d \
+    --restart always \
+    -p 8000:8000 \
+    $DOCKER_ARGS \
+    --mount source=blobs,destination=/blobs \
+    $DOCKER_IMAGE
