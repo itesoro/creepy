f62233efc45c7b98922877465ceeda32d7a1dfad
Roman Rizvanov <progbeat@gmail.com>
2021-04-19 21:51:15 +0300

    Support path-like objects in serialization.py.

--- creepy/serialization.py
+++ creepy/serialization.py
@@ -1,5 +1,7 @@
 import os
-from typing import Optional, BinaryIO
+import pathlib
+from typing import Optional
+from contextlib import ExitStack
 
 from cryptography.hazmat import backends
 from cryptography.hazmat.primitives import serialization
@@ -31,11 +33,12 @@ _id_filenames = ['id_rsa', 'id_dsa', 'id_ecdsa', 'id_ed25519']
 
 def _find_key(path: Optional[str], ext: str = '', ssh_dir: Optional[str] = None) -> str:
     if path is not None:
-        return os.path.expanduser(path)
+        return pathlib.Path(path).expanduser()
     if ssh_dir is None:
-        ssh_dir = os.path.expanduser('~/.ssh')
+        ssh_dir = '~/.ssh'
+    ssh_dir = pathlib.Path(ssh_dir).expanduser()
     for id_filename in _id_filenames:
-        path = os.path.join(ssh_dir, id_filename + ext)
+        path = ssh_dir / (id_filename + ext)
         if os.path.isfile(path):
             return path
 
@@ -58,7 +61,7 @@ def load_private_key(path=None, passphrase: Optional[SecureString] = None, ssh_d
     path = _find_key(path, ssh_dir=ssh_dir)
     if path is None:
         raise RuntimeError('Failed to find private key file')
-    session = connect('_detail/private_key', '008eac7db2d0dd695e37e705a94761176016474824885217f7d42aa04fa8904b')
+    session = connect('_detail/private_key', '8ea56fd9022fa63c87876e167d5d2a5c149144738f476411f3d5348e07e9f4f4')
     session.request('load', path, passphrase)
     return ProcessifiedPrivateKey(session)
 
@@ -72,18 +75,19 @@ def load_public_key(file=None, ssh_dir: Optional[str] = None):
     ssh_dir: string or pathlib.Path, optional
         If `path` is not specified public key file is searched in `ssh_dir` directory.
     """
-    if file is None or isinstance(file, str):
-        path = _find_key(file, '.pub', ssh_dir=ssh_dir)
-        if path is None:
-            raise RuntimeError('Failed to find public key file')
-        with open(path, 'rb') as f:
-            return load_public_key(f)
-    key_bytes = file if isinstance(file, bytes) else file.read()
+    if not isinstance(file, bytes):
+        try:
+            file = _find_key(file, '.pub', ssh_dir=ssh_dir)
+            if file is None:
+                raise RuntimeError('Failed to find public key file')
+        except TypeError:
+            pass
+        file = file.read_bytes()
     loaders = [serialization.load_pem_public_key, serialization.load_ssh_public_key]
     backend = backends.default_backend()
     for loader in loaders:
         try:
-            return loader(key_bytes, backend=backend)
+            return loader(file, backend=backend)
         except (TypeError, ValueError):
             pass
     return ValueError('Invalid key format')
@@ -100,25 +104,29 @@ def dump_private_key(private_key, file) -> SecureString:
         Depending on type treated as file object or path to a destination file.
     """
     from creepy.utils import processify
-
-    if isinstance(file, str):
-        with open(file, 'wb') as f:
-            return dump_private_key(private_key, f)
     from cryptography.hazmat.primitives import serialization
-    passphrase = SecureString.random(16)
+    from creepy.protocol import asymmetric
 
-    @processify  # run in separate process so passphrase bytes are exposed only during writting a key to a file
-    def doit():
-        with passphrase as passphrase_mem:
-            passphrase_bytes = bytes(passphrase_mem)
-            private_key_bytes = private_key.private_bytes(
-                encoding=serialization.Encoding.PEM,
-                format=serialization.PrivateFormat.OpenSSH,
-                encryption_algorithm=serialization.BestAvailableEncryption(passphrase_bytes)
-            )
-            file.write(private_key_bytes)
+    passphrase = asymmetric.generate_passphrase()
+    with ExitStack() as stack:
+        try:
+            file = stack.enter_context(open(file, 'wb'))
+        except TypeError:
+            pass
 
-    doit()
+        @processify  # run in separate process so passphrase bytes are exposed only during writting a key to a file
+        def doit():
+            with passphrase as passphrase_mem:
+                passphrase_bytes = bytes(passphrase_mem)
+                private_key_bytes = private_key.private_bytes(
+                    encoding=serialization.Encoding.PEM,
+                    format=serialization.PrivateFormat.PKCS8,
+                    encryption_algorithm=serialization.BestAvailableEncryption(passphrase_bytes)
+                )
+                file.write(private_key_bytes)
+                file.flush()
+
+        doit()
     return passphrase
 
 
@@ -132,11 +140,14 @@ def dump_public_key(public_key, file):
     file: file-like object, string, or pathlib.Path
         File or filename to which the public key is dumped.
     """
-    if not isinstance(file, BinaryIO):
-        with open(file, 'wb') as f:
-            return dump_public_key(public_key, f)
-    public_key_bytes = public_key.public_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.OpenSSH
-    )
-    file.write(public_key_bytes)
+    with ExitStack() as stack:
+        try:
+            file = stack.enter_context(open(file, 'wb'))
+        except TypeError:
+            pass
+        public_key_bytes = public_key.public_bytes(
+            encoding=serialization.Encoding.OpenSSH,
+            format=serialization.PublicFormat.OpenSSH
+        )
+        file.write(public_key_bytes)
+        file.flush()
