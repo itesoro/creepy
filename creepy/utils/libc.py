9db3edc436ae25c00a41c2ade0c92a90e1504982
Roman Rizvanov <progbeat@gmail.com>
2021-04-19 15:43:06 +0300

    Minor fixes.

--- creepy/utils/libc.py
+++ creepy/utils/libc.py
@@ -11,24 +11,24 @@ _libc = ctypes.CDLL('libc.so.6', use_errno=True)
 
 
 def mlockall(flags: int = MCL_CURRENT | MCL_FUTURE):
-    result = _libc.mlockall(c_int(flags))
-    if result != 0:
-        raise OSError(ctypes.get_errno(), 'Cannot lock memmory')
+    if _libc.mlockall(c_int(flags)) == 0:
+        return
+    raise OSError(ctypes.get_errno(), 'Cannot lock memmory')
 
 
 def munlockall():
-    result = _libc.munlockall()
-    if result != 0:
-        raise OSError(ctypes.get_errno(), 'Cannot unlock memmory')
+    if _libc.munlockall() == 0:
+        return
+    raise OSError(ctypes.get_errno(), 'Cannot unlock memmory')
 
 
 def mlock(addr, size):
-    result = _libc.mlock(c_void_p(addr), c_size_t(size))
-    if result != 0:
-        raise OSError(ctypes.get_errno(), 'Cannot lock memmory')
+    if _libc.mlock(c_void_p(addr), c_size_t(size)) == 0:
+        return
+    raise OSError(ctypes.get_errno(), 'Cannot lock memmory')
 
 
 def munlock(addr, size):
-    result = _libc.munlock(c_void_p(addr), c_size_t(size))
-    if result != 0:
-        raise OSError(ctypes.get_errno(), 'Cannot unlock memmory')
+    if _libc.munlock(c_void_p(addr), c_size_t(size)) == 0:
+        return
+    raise OSError(ctypes.get_errno(), 'Cannot unlock memmory')
