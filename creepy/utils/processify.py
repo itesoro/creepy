92836b1b0182f0c2c7935c2dd2b046a32f00ba6b
Anton Orlov <ion6431@gmail.com>
2022-07-29 11:34:08 +0400

    Fix processify if start method is spawn (#54)
    * Fix processify if start method is spawn
    
    * Use multiprocessing pipes

--- creepy/utils/processify.py
+++ creepy/utils/processify.py
@@ -1,11 +1,12 @@
 import os
 import time
-import pickle
 import signal
 import struct
 import functools
-from multiprocessing import Process
+from multiprocessing import Process, Pipe
+from multiprocessing.connection import Connection as PipeConnection
 from threading import Thread
+from typing import Callable
 
 
 def processify(fn):
@@ -16,26 +17,14 @@ def processify(fn):
     ----
     It doesn't encrypt communications with a child process.
     """
-    from creepy.subprocess import common
-
     @functools.wraps(fn)
     def wrapper(*args, **kwargs):
-        def job(ppid: int, out_fd: int):
-            Thread(target=_suicide_when_orphan, args=(ppid,), daemon=True).start()
-            send = common.make_send(out_fd)
-            try:
-                result = (fn(*args, **kwargs), None)
-            except Exception as e:
-                result = (None, e)
-            send(pickle.dumps(result))
-
-        in_fd, out_fd = os.pipe()
-        recv = common.make_recv(in_fd)
-        job_process = Process(target=job, args=(os.getpid(), out_fd))
+        in_connection, out_connection = Pipe()
+        job_process = Process(target=_job, args=(os.getpid(), out_connection, fn, args, kwargs))
         job_process.start()
-        Thread(target=_join_close, args=(job_process, out_fd), daemon=True).start()
+        Thread(target=_join_close, args=(job_process, out_connection), daemon=True).start()
         try:
-            result, exception = pickle.loads(recv())
+            result, exception = in_connection.recv()
         except struct.error:  # happens when `_join_close()` closes `out_fd`
             raise RuntimeError(f'Process running {fn} exited with code {job_process.exitcode}') from None
         if exception is not None:
@@ -52,7 +41,16 @@ def _suicide_when_orphan(ppid: int):
     os.kill(os.getpid(), signal.SIGKILL)  # suicide
 
 
-def _join_close(process: Process, fd: int):
+def _join_close(process: Process, connection: PipeConnection):
     """Close file descriptor `fd` after `process` is done."""
     process.join()
-    os.close(fd)
+    connection.close()
+
+
+def _job(ppid: int, out_connection: PipeConnection, fn: Callable, args: tuple, kwargs: dict):
+    Thread(target=_suicide_when_orphan, args=(ppid,), daemon=True).start()
+    try:
+        result = (fn(*args, **kwargs), None)
+    except Exception as e:
+        result = (None, e)
+    out_connection.send(result)
