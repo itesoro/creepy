057e76c0a5db7b14dfc8d7fa975a0e2fc1740755
Anton Orlov <ion6431@gmail.com>
2023-06-09 14:03:32 +0200

    Rollback AES, use it as default

--- creepy/protocol/common.py
+++ creepy/protocol/common.py
@@ -24,9 +24,32 @@ class ChaCha20Poly1305:
         return self._cipher.decrypt(ciphertext[:self.NONCE_BYTES], ciphertext[self.NONCE_BYTES:], b'')
 
 
+class AES256GCM:
+    KEY_BITS = 256
+    NONCE_BYTES = 16
+
+    @property
+    def name(self):
+        return type(self).__name__
+
+    def __init__(self, key):
+        assert len(key) * 8 == self.KEY_BITS
+        self._cipher = aead.AESGCM(key)
+        self.key = key
+
+    def encrypt(self, message):
+        nonce = secrets.token_bytes(self.NONCE_BYTES)
+        ciphertext = nonce + self._cipher.encrypt(nonce, message, b'')
+        return ciphertext
+
+    def decrypt(self, ciphertext):
+        return self._cipher.decrypt(ciphertext[:self.NONCE_BYTES], ciphertext[self.NONCE_BYTES:], b'')
+
+
 def make_cipher(algo_name, key=None):
     _symmetric_algos = {
         'ChaCha20Poly1305': ChaCha20Poly1305,
+        'AES256GCM': AES256GCM,
     }
     algo = _symmetric_algos.get(algo_name)
     if algo is None:
